85eb26f3a403878f3538fda90e690f843d9b05bf
trsaints <thiago.dev2k3@gmail.com>
2025-09-04 19:10:41 -0300

    feat: create API layer for specifying dto models and route controllers

---
+++ AluraIdentity6Api/Api/RequestModels/CreateUsuarioModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AluraIdentity6Api.Api.RequestModels;
+
+public record CreateUsuarioModel
+{
+    [Required]
+    public string? UserName { get; init; }
+
+    [Required]
+    public DateTime DataNascimento { get; init; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string? Password { get; init; }
+
+    [Required]
+    [Compare(nameof(Password))]
+    public string? ConfirmPassword { get; init; }
+}
