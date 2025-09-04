28c46dd7cb9a2075faa5cf1e6427243f5c3b7e69
trsaints <thiago.dev2k3@gmail.com>
2025-09-04 19:11:44 -0300

    feat: create infrastructure layer for prodiving context translation between api and domain layer

---
+++ AluraIdentity6Api/Infra/Data/Mappers/Interfaces/IMapper.cs
@@ -0,0 +1,8 @@
+﻿namespace AluraIdentity6Api.Infra.Data.Mappers.Interfaces;
+
+
+public interface IMapper<D, R> where D : class
+{
+    public D? ToDomainModel(R value);
+    public R? ToRequestModel(D model);
+}
