353d5d5e31d2c610fbe6cdd57ff777519d934946
trsaints <thiago.dev2k3@gmail.com>
2025-09-04 19:12:27 -0300

    feat: provide base structure for implementing a domain/dto translator for AppUser models

---
+++ AluraIdentity6Api/Infra/Data/Mappers/UsuarioMapper.cs
@@ -0,0 +1,26 @@
+﻿using AluraIdentity6Api.Api.RequestModels;
+using AluraIdentity6Api.App.Data.Models;
+using AluraIdentity6Api.Infra.Data.Mappers.Interfaces;
+
+namespace AluraIdentity6Api.Infra.Data.Mappers;
+
+public class UsuarioMapper : IMapper<AppUser, CreateUsuarioModel>
+{
+    public AppUser? ToDomainModel(CreateUsuarioModel value)
+    {
+        AppUser result = new()
+        {
+        };
+
+        return result;
+    }
+
+    public CreateUsuarioModel? ToRequestModel(AppUser model)
+    {
+        CreateUsuarioModel result = new()
+        {
+        };
+
+        return result;
+    }
+}
