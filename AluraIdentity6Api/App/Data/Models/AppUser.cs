ddd9c52b6838629034a86fee506d7e03219b1265
trsaints <thiago.dev2k3@gmail.com>
2025-09-04 19:10:12 -0300

    feat: create base model for domain application user

---
+++ AluraIdentity6Api/App/Data/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using AluraIdentity6Api.App.Data.Models.Interfaces;
+
+namespace AluraIdentity6Api.App.Data.Models;
+
+public class AppUser : ITrackable
+{
+    public string? Name { get; set; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; set; }
+}
