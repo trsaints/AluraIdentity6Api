c475419d9ef665b437138db3e42a6e920192cedd
trsaints <thiago.dev2k3@gmail.com>
2025-09-04 19:10:59 -0300

    feat: create base controller for handling AppUser operations

---
+++ AluraIdentity6Api/Api/Controllers/UsuariosController.cs
@@ -0,0 +1,15 @@
+﻿using AluraIdentity6Api.Api.RequestModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AluraIdentity6Api.Api.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class UsuariosController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Cadastrar([FromBody] CreateUsuarioModel model)
+    {
+        throw new NotImplementedException();
+    }
+}
