808c8eb64740c253d37a800a33c0668cd94a688e
trsaints <thiago.dev2k3@gmail.com>
2025-09-04 19:07:03 -0300

    feat: create domain layer for specifying entity models

---
+++ AluraIdentity6Api/App/Data/Models/Entity.cs
@@ -0,0 +1,13 @@
+﻿using AluraIdentity6Api.App.Data.Models.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace AluraIdentity6Api.App.Data.Models;
+
+public abstract class Entity : ITrackable
+{
+    [Key]
+    public int Id { get; set; }
+    public virtual string? Name { get ; set ; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ AluraIdentity6Api/App/Data/Models/Interfaces/ITrackable.cs
@@ -0,0 +1,8 @@
+﻿namespace AluraIdentity6Api.App.Data.Models.Interfaces;
+
+public interface ITrackable
+{
+    public string? Name { get; set; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; set; }
+}
