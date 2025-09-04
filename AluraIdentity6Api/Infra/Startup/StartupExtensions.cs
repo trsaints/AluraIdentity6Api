e28a5fa4582ed4bae0854d1a672c349336886ded
trsaints <thiago.dev2k3@gmail.com>
2025-09-04 19:19:13 -0300

    feat: provide extension utility for configuring application's dependency pipeline

---
+++ AluraIdentity6Api/Infra/Startup/StartupExtensions.cs
@@ -0,0 +1,14 @@
+﻿using AluraIdentity6Api.Api.RequestModels;
+using AluraIdentity6Api.App.Data.Models;
+using AluraIdentity6Api.Infra.Data.Mappers;
+using AluraIdentity6Api.Infra.Data.Mappers.Interfaces;
+
+namespace AluraIdentity6Api.Infra.Startup;
+
+public static class StartupExtensions
+{
+    public static void AddDataMappers(this IServiceCollection services)
+    {
+        services.AddSingleton<IMapper<AppUser, CreateUserModel>, UserMapper>();
+    }
+}
