77452e7c0ac6b5c92ae2e09256a1eefa6bac5eef
trsaints <thiago.dev2k3@gmail.com>
2025-09-07 12:57:22 -0300

    feat: create custom database context

---
+++ AluraIdentity6Api/Infra/Data/Database/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AluraIdentity6Api.App.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AluraIdentity6Api.Infra.Data.Database;
+
+public class AppDbContext : IdentityDbContext<AppUser, IdentityRole<int>, int>
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
