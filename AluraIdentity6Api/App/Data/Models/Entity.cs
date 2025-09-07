9de73f2764a7e857d7cfa2892b87604284ad4a35
trsaints <thiago.dev2k3@gmail.com>
2025-09-07 12:20:44 -0300

    update: ensure primary key may not be updated

--- AluraIdentity6Api/App/Data/Models/Entity.cs
+++ AluraIdentity6Api/App/Data/Models/Entity.cs
@@ -6,7 +6,7 @@ namespace AluraIdentity6Api.App.Data.Models;
 public abstract class Entity : ITrackable
 {
     [Key]
-    public int Id { get; set; }
+    public int Id { get; init; }
     public virtual string? Name { get ; set ; }
     public DateTime CreatedAt { get; init; }
     public DateTime UpdatedAt { get; set; }
