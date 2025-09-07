7eb162a1c02ebf134bf167768944bcd4deef2882
trsaints <thiago.dev2k3@gmail.com>
2025-09-07 12:36:01 -0300

    update: override contract member to enforce it should not be used

--- AluraIdentity6Api/App/Data/Models/AppUser.cs
+++ AluraIdentity6Api/App/Data/Models/AppUser.cs
@@ -1,5 +1,6 @@
 ﻿using AluraIdentity6Api.App.Data.Models.Interfaces;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace AluraIdentity6Api.App.Data.Models;
 
@@ -10,4 +11,8 @@ public class AppUser : IdentityUser<string>, ITrackable
     public string? FullName { get; set; }
     public DateTime CreatedAt { get; init; }
     public DateTime UpdatedAt { get; set; }
+    public string? Name { 
+        get => throw new NotImplementedException(); 
+        set => throw new NotImplementedException(); 
+    }
 }
