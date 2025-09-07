c255ffec01ec7d59387780a61d4f59e0a965cd77
trsaints <thiago.dev2k3@gmail.com>
2025-09-07 13:02:42 -0300

    feat: provide custom database context configuration on program startup level

---
+++ AluraIdentity6Api/Infra/Data/Constants/AppDbConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace AluraIdentity6Api.Infra.Data.Constants;
+
+public static class AppDbConstants
+{
+    public const string DefaultConnectionStringName = "DefaultConnection";
+}
--- AluraIdentity6Api/Infra/Startup/StartupExtensions.cs
+++ AluraIdentity6Api/Infra/Startup/StartupExtensions.cs
@@ -1,7 +1,10 @@
 ﻿using AluraIdentity6Api.Api.RequestModels;
 using AluraIdentity6Api.App.Data.Models;
+using AluraIdentity6Api.Infra.Data.Constants;
+using AluraIdentity6Api.Infra.Data.Database;
 using AluraIdentity6Api.Infra.Data.Mappers;
 using AluraIdentity6Api.Infra.Data.Mappers.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace AluraIdentity6Api.Infra.Startup;
 
@@ -10,5 +13,13 @@ public static class StartupExtensions
     public static void AddDataMappers(this IServiceCollection services)
     {
         services.AddSingleton<IMapper<AppUser, CreateUserModel>, UserMapper>();
+    }
+
+    public static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString(AppDbConstants.DefaultConnectionStringName) 
+            ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+        services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
     }
 }
