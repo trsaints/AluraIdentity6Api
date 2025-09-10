c08f9ce045dd5aac2eb2bcfed1ba19a1e8d2b382
trsaints <thiago.dev2k3@gmail.com>
2025-09-09 21:06:04 -0300

    feat: create abstraction for providing identity-similar structure of service-layer operations

---
+++ AluraIdentity6Api/App/Data/Models/Interfaces/IServiceResult.cs
@@ -0,0 +1,10 @@
+﻿namespace AluraIdentity6Api.App.Data.Models.Interfaces;
+
+public interface IServiceResult<T>
+{
+    bool Succeeded { get; }
+    IEnumerable<string>? Errors { get; }
+    T? Data { get; }
+    static abstract IServiceResult<T> Ok(T data);
+    static abstract IServiceResult<T> Fail(IEnumerable<string> errors);
+}
---
+++ AluraIdentity6Api/App/Data/Models/ServiceResult.cs
@@ -0,0 +1,32 @@
+﻿using AluraIdentity6Api.App.Data.Models.Interfaces;
+
+namespace AluraIdentity6Api.App.Data.Models;
+
+public class ServiceResult<T> : IServiceResult<T>
+{
+    public bool Succeeded { get; init; }
+
+    public IEnumerable<string>? Errors { get; init; }
+
+    public T? Data { get; init; }
+
+    public static IServiceResult<T> Fail(IEnumerable<string> errors)
+    {
+        return new ServiceResult<T>()
+        {
+            Succeeded = false,
+            Data = default,
+            Errors = errors
+        };
+    }
+
+    public static IServiceResult<T> Ok(T data)
+    {
+        return new ServiceResult<T>()
+        {
+            Succeeded = true,
+            Data = data,
+            Errors = null
+        };
+    }
+}
