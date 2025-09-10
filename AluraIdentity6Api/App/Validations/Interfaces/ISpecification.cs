07583df60d4132f1060ac69564af32dfb9e9e1d3
trsaints <thiago.dev2k3@gmail.com>
2025-09-09 22:06:57 -0300

    feat: provide generic 'Specification' contract for further implementations

---
+++ AluraIdentity6Api/App/Validations/Interfaces/ISpecification.cs
@@ -0,0 +1,8 @@
+﻿using AluraIdentity6Api.App.Data.Models.Interfaces;
+
+namespace AluraIdentity6Api.App.Validations.Interfaces;
+
+public interface ISpecification<T> where T : ITrackable
+{
+    bool IsSatisfiedBy(T entity);
+}
