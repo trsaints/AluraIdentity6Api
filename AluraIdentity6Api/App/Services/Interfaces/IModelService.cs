1bd1e8eb9476ee566b62d56ad1f97e57962a5bf6
trsaints <thiago.dev2k3@gmail.com>
2025-09-09 21:10:18 -0300

    feat: provide base skeleton of service layer implementations

---
+++ AluraIdentity6Api/App/Services/Interfaces/IModelService.cs
@@ -0,0 +1,13 @@
+﻿using AluraIdentity6Api.App.Data.Models;
+using AluraIdentity6Api.App.Data.Models.Interfaces;
+
+namespace AluraIdentity6Api.App.Services.Interfaces;
+
+public interface IModelService<T> where T : ITrackable
+{
+    Task<ServiceResult<T>> CreateAsync(T entity);
+    Task<ServiceResult<T>> UpdateAsync(T entity);
+    Task<ServiceResult<T>> DeleteAsync(int id);
+    Task<ServiceResult<T>> GetByIdAsync(int id);
+    Task<ServiceResult<IEnumerable<T>>> GetAllAsync();
+}
---
+++ AluraIdentity6Api/App/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using AluraIdentity6Api.App.Data.Models;
+using AluraIdentity6Api.App.Services.Interfaces;
+
+namespace AluraIdentity6Api.App.Services;
+
+public class UserService : IModelService<AppUser>
+{
+    public Task<ServiceResult<AppUser>> CreateAsync(AppUser entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResult<AppUser>> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResult<IEnumerable<AppUser>>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResult<AppUser>> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResult<AppUser>> UpdateAsync(AppUser entity)
+    {
+        throw new NotImplementedException();
+    }
+}
