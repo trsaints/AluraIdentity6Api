2c58492e335cd3e50c41ce8477aae55526f448ca
trsaints <thiago.dev2k3@gmail.com>
2025-09-21 17:41:03 -0300

    feat(Infra): provide utility for configuring JWT authentication parameters

---
+++ AluraIdentity6Api/Infra/Settings/JwtSettings.cs
@@ -0,0 +1,31 @@
+﻿using AluraIdentity6Api.Infra.Data.Constants;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace AluraIdentity6Api.Infra.Settings;
+
+public static class JwtSettings
+{
+    public static TokenValidationParameters TokenValidationParameters()
+    {
+        var jwtIssuer = Environment.GetEnvironmentVariable(EnvironmentVariables.JWT_ISSUER)
+            ?? throw new ApplicationException("JWT_ISSUER não configurada");
+
+        var jwtAudience = Environment.GetEnvironmentVariable(EnvironmentVariables.JWT_AUDIENCE)
+            ?? throw new ApplicationException("JWT_AUDIENCE não configurada");
+
+        var jwtKey = Environment.GetEnvironmentVariable(EnvironmentVariables.JWT_KEY)
+            ?? throw new InvalidOperationException("JWT_KEY não configurada");
+
+        return new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtAudience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+    }
+}
